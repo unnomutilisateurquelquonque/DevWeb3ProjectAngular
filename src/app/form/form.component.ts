117fc5157711eeac791f512461403500cb79d56e
Maxime <maxdemersboucher@gmail.com>
2022-12-23 11:23:50 -0500

    Ajout des messages d'erreur a l'usager

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -9,34 +9,9 @@ import { PersonaService } from '../persona.service';
 })
 
 export class FormComponent {
-  messageErreur="";
+  messageErreur=["","","","","",""];
   personas: Persona[] = [];
- /* personaForm = new FormGroup({
-    lv: new FormControl(''),
-    lastName: new FormControl(''),
-    arcane: new FormControl(''),
-    stats: new FormGroup({
-      competances: new FormGroup({
-        force: new FormControl(''),
-        magique: new FormControl(''),
-        endurance: new FormControl(''),
-        resistance: new FormControl(''),
-        chance: new FormControl('')
-      }),
-      faibless: new FormGroup({
-        physique: new FormControl(''),
-        fusil: new FormControl(''),
-        feu: new FormControl(''),
-        glace: new FormControl(''),
-        electrique: new FormControl(''),
-        vent: new FormControl(''),
-        psychique: new FormControl(''),
-        nucleaire: new FormControl(''),
-        divin: new FormControl(''),
-        malediction: new FormControl(''),
-      }),
-    })
-  });*/
+  erreur = false;
   nouvellePersona: Persona = {
     lv:0,
     nom:"",
@@ -69,7 +44,92 @@ export class FormComponent {
   constructor(private personaService: PersonaService ) {}
 
   onSubmit() {
+    this.erreur = false;
+    if(this.nouvellePersona.lv < 1  || this.nouvellePersona.lv > 99 ){
+      this.messageErreur[0] = "Le champ lv doit être entre 1 et 99";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.nom == ""){
+      this.messageErreur[1] = "Le champ nom ne peut pas être vide";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.arcane == ""){
+      this.messageErreur[2] = "Le champ arcane ne peut pas être vide";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.competance.force < 1 || this.nouvellePersona.stats.competance.force > 99){
+      this.messageErreur[3] = "Les champs dans competance doivent être entre 1 et 99";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.competance.magique < 1 || this.nouvellePersona.stats.competance.magique > 99){
+      this.messageErreur[3] = "Les champs dans competance doivent être entre 1 et 99";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.competance.endurance < 1 || this.nouvellePersona.stats.competance.endurance > 99){
+      this.messageErreur[3] = "Les champs dans competance doivent être entre 1 et 99";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.competance.agilite < 1 || this.nouvellePersona.stats.competance.agilite > 99){
+      this.messageErreur[3] = "Les champs dans competance doivent être entre 1 et 99";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.competance.chance < 1 || this.nouvellePersona.stats.competance.chance > 99){
+      this.messageErreur[3] = "Les champs dans competance doivent être entre 1 et 99";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.faibless.physique != "","absorbe","nullifier","resist","deflecte"){
+      this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.faibless.fusil != "","absorbe","nullifier","resist","deflecte"){
+      this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.faibless.feu != "","absorbe","nullifier","resist","deflecte"){
+      this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.faibless.glace != "","absorbe","nullifier","resist","deflecte"){
+      this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.faibless.electrique != "","absorbe","nullifier","resist","deflecte"){
+      this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.faibless.vent != "","absorbe","nullifier","resist","deflecte"){
+      this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.faibless.psychique != "","absorbe","nullifier","resist","deflecte"){
+      this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.faibless.nucleaire != "","absorbe","nullifier","resist","deflecte"){
+      this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.faibless.divin != "","absorbe","nullifier","resist","deflecte"){
+      this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+      this.erreur = true;
+    }
+    if(this.nouvellePersona.stats.faibless.maledition != "","absorbe","nullifier","resist","deflecte"){
+      this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+      this.erreur = true;
+    }
+    if((this.nouvellePersona.obtenue) != true,false){
+      this.messageErreur[5] = "Le champ obtenue prend seulement les valeurs true ou false";
+      this.erreur = true;
+    }
+    if(this.erreur == false){
+      this.messageErreur[0] = "";
+      this.messageErreur[1] = "";
+      this.messageErreur[2] = "";
+      this.messageErreur[3] = "";
+      this.messageErreur[4] = "";
+      this.messageErreur[5] = "";
       this.personaService.addPersona(this.nouvellePersona)
           .subscribe(persona  => { this.personas.push(persona);});
+    }
   }
 }
--- src/app/modifier-persona/modifier-persona.component.ts
+++ src/app/modifier-persona/modifier-persona.component.ts
@@ -9,6 +9,8 @@ import { PersonaService } from '../persona.service';
 })
 
 export class ModifierPersonaComponent {
+  messageErreur=["","","","","",""];
+  erreur = false;
   id = localStorage.getItem('personaId');
   nom = localStorage.getItem('personaNom');
   lv = localStorage.getItem('personaLv');
@@ -31,32 +33,6 @@ export class ModifierPersonaComponent {
   divin = localStorage.getItem('personaFaiblessDivin');
   maledition =  localStorage.getItem('personaFaiblessMaledition');
   personas: Persona[] = [];
- /* personaForm = new FormGroup({
-    lv: new FormControl(''),
-    lastName: new FormControl(''),
-    arcane: new FormControl(''),
-    stats: new FormGroup({
-      competances: new FormGroup({
-        force: new FormControl(''),
-        magique: new FormControl(''),
-        endurance: new FormControl(''),
-        resistance: new FormControl(''),
-        chance: new FormControl('')
-      }),
-      faibless: new FormGroup({
-        physique: new FormControl(''),
-        fusil: new FormControl(''),
-        feu: new FormControl(''),
-        glace: new FormControl(''),
-        electrique: new FormControl(''),
-        vent: new FormControl(''),
-        psychique: new FormControl(''),
-        nucleaire: new FormControl(''),
-        divin: new FormControl(''),
-        malediction: new FormControl(''),
-      }),
-    })
-  });*/
   personaModifier: Persona = {
     lv:0,
     nom:"",
@@ -92,12 +68,93 @@ export class ModifierPersonaComponent {
 
     }
   
-  onSubmit() {
-    if(this.id==null){
-      this.id = "0";
+    onSubmit() {
+      this.erreur = false;
+      if(this.personaModifier.lv < 1  || this.personaModifier.lv > 99 ){
+        this.messageErreur[0] = "Le champ lv doit être entre 1 et 99";
+        this.erreur = true;
+      }
+      if(this.personaModifier.nom == ""){
+        this.messageErreur[1] = "Le champ nom ne peut pas être vide";
+        this.erreur = true;
+      }
+      if(this.personaModifier.arcane == ""){
+        this.messageErreur[2] = "Le champ arcane ne peut pas être vide";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.competance.force < 1 || this.personaModifier.stats.competance.force > 99){
+        this.messageErreur[3] = "Les champs dans competance doivent être entre 1 et 99";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.competance.magique < 1 || this.personaModifier.stats.competance.magique > 99){
+        this.messageErreur[3] = "Les champs dans competance doivent être entre 1 et 99";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.competance.endurance < 1 || this.personaModifier.stats.competance.endurance > 99){
+        this.messageErreur[3] = "Les champs dans competance doivent être entre 1 et 99";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.competance.agilite < 1 || this.personaModifier.stats.competance.agilite > 99){
+        this.messageErreur[3] = "Les champs dans competance doivent être entre 1 et 99";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.competance.chance < 1 || this.personaModifier.stats.competance.chance > 99){
+        this.messageErreur[3] = "Les champs dans competance doivent être entre 1 et 99";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.faibless.physique != "","absorbe","nullifier","resist","deflecte"){
+        this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.faibless.fusil != "","absorbe","nullifier","resist","deflecte"){
+        this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.faibless.feu != "","absorbe","nullifier","resist","deflecte"){
+        this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.faibless.glace != "","absorbe","nullifier","resist","deflecte"){
+        this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.faibless.electrique != "","absorbe","nullifier","resist","deflecte"){
+        this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.faibless.vent != "","absorbe","nullifier","resist","deflecte"){
+        this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.faibless.psychique != "","absorbe","nullifier","resist","deflecte"){
+        this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.faibless.nucleaire != "","absorbe","nullifier","resist","deflecte"){
+        this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.faibless.divin != "","absorbe","nullifier","resist","deflecte"){
+        this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+        this.erreur = true;
+      }
+      if(this.personaModifier.stats.faibless.maledition != "","absorbe","nullifier","resist","deflecte"){
+        this.messageErreur[4] = "Les champs dans faibless doivent être l'une de ces valeur: vide,absorbe,nullifier,resist,deflecte";
+        this.erreur = true;
+      }
+      if((this.personaModifier.obtenue) != true,false){
+        this.messageErreur[5] = "Le champ obtenue prend seulement les valeurs true ou false";
+        this.erreur = true;
+      }
+      if(this.erreur == false){
+        this.messageErreur[0] = "";
+        this.messageErreur[1] = "";
+        this.messageErreur[2] = "";
+        this.messageErreur[3] = "";
+        this.messageErreur[4] = "";
+        this.messageErreur[5] = "";
+        this.personaService.addPersona(this.personaModifier)
+            .subscribe(persona  => { this.personas.push(persona);});
+      }
     }
-    console.log(this.personaModifier);
-      this.personaService.updatePersona(this.personaModifier,this.id)
-          .subscribe(persona  => { this.personas.push(persona);});
-  }
 }
